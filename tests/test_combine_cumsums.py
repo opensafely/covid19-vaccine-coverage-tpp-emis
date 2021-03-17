b14f13dd24adc87ea920e9912cc4df45ed2d987b
Peter Inglesby <peter.inglesby@gmail.com>
2021-03-17 09:35:26 +0000

    Account for differing columns when combining cumsums

--- tests/test_combine_cumsums.py
+++ tests/test_combine_cumsums.py
@@ -8,19 +8,44 @@ from analysis.transform import load_raw_cohort, transform
 def test_combine_cumsums():
     raw_cohort = load_raw_cohort("tests/input.csv")
     cohort = transform(raw_cohort)
-    uptake = compute_uptake(cohort, "vacc1_dat", "sex")
-    # Drop the last two non-total rows
-    uptake1 = uptake.drop(uptake.iloc[-3:-1].index)
-    # Drop the first two rows
-    uptake2 = uptake.drop(uptake.iloc[:2].index)
+    uptake = compute_uptake(cohort, "vacc1_dat", "imd_band")
+
+    # Drop the last two non-total rows and last column
+    uptake1 = uptake.drop(uptake.iloc[-3:-1].index).loc[:, 0:4]
+    # Drop the first two rows and first column
+    uptake2 = uptake.drop(uptake.iloc[:2].index).loc[:, 1:5]
+
     combined = combine_cumsums(uptake1, uptake2)
 
     assert_index_equal(combined.index, uptake.index)
+    assert list(combined.columns) == [0, 1, 2, 3, 4, 5]
 
     num_date_rows = len(uptake) - 1
+
+    # Check that all items in first column come from uptake1
+    for ix in range(num_date_rows):
+        if ix < num_date_rows - 2:
+            assert combined[0][ix] == uptake1[0][ix]
+        else:
+            assert combined[0][ix] == uptake1[0][-2]
+    assert combined[0]["total"] == uptake1[0]["total"]
+
+    # Check that all items in last column come from uptake2
     for ix in range(num_date_rows):
         if ix < 2:
-            # Check the first two rwos come from uptake1
+            assert combined[5][ix] == 0
+        else:
+            assert combined[5][ix] == uptake2[5][ix - 2]
+    assert combined[5]["total"] == uptake2[5]["total"]
+
+    combined = combined.loc[:, 1:4]
+    uptake1 = uptake1.loc[:, 1:4]
+    uptake2 = uptake2.loc[:, 1:4]
+
+    # Check all other columns
+    for ix in range(num_date_rows):
+        if ix < 2:
+            # Check the first two rows come from uptake1
             assert_series_equal(combined.iloc[ix], uptake1.iloc[ix])
         elif ix < num_date_rows - 2:
             assert_series_equal(
